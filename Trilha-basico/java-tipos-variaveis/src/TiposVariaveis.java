19ced258322844f67ed03ab39fee4659373b368f
Bootcamp-JAVA <breno.alvesdx@gmail.com>
2025-02-24 23:21:01 -0300

    feat: adicionando tipos e variáveis

--- Trilha-basico/java-tipos-variaveis/src/TiposVariaveis.java
+++ Trilha-basico/java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+        double salarioMinimo = 2500;
+        float salarioFloat = 2500f;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Constantes
+        int numero = 5;
+        numero = 10;
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 10.5; Valor não pode ser alterado!
+
+        System.out.println("Variável número: " + numero);
+        System.out.println("Constante: " + VALOR_DE_PI);
+
+        //String e Char
+        String meuNome = "Breno Alves";
+        char genero = 'M';
+
+        System.out.println("Nome: " + meuNome);
+        System.out.println("Gênero: " + genero);
     }
 }
