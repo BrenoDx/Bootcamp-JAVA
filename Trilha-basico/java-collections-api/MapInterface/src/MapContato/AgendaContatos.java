31ca0af0cc10d63a3b7f43a1d26b6cd4ba6cde81
Bootcamp-JAVA <breno.alvesdx@gmail.com>
2025-03-18 10:59:46 -0300

    Feat: adicionando os métodos AgendaContatos

--- Trilha-basico/java-collections-api/MapInterface/src/MapContato/AgendaContatos.java
+++ Trilha-basico/java-collections-api/MapInterface/src/MapContato/AgendaContatos.java
@@ -4,10 +4,40 @@
  */
 package MapContato;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author breno
  */
 public class AgendaContatos {
+    private Map<String, Integer> mapAgenda;
+
+    public AgendaContatos() {
+        this.mapAgenda = new HashMap<>();
+    }
     
+    public void adicionarContato(String nome, int telefone){
+        mapAgenda.put(nome, telefone);
+    }
+    
+    public void removerContato(String nome){
+        if(!mapAgenda.isEmpty()){
+            mapAgenda.remove(nome);
+        }
+    }
+    
+    public void exibirContato(){
+            System.out.println(mapAgenda);
+    }
+    
+    public int pesquisarPorNome(String nome){
+        Integer numero = null;
+        if(!mapAgenda.isEmpty()){
+           numero = mapAgenda.get(nome);
+            
+        }
+        return numero;
+    }
 }
