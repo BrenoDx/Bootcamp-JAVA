41b7e928e70c9c0648d07c067452494102db9c0c
Bootcamp-JAVA <breno.alvesdx@gmail.com>
2025-03-18 11:16:47 -0300

    Test: teste dos métodos AgendaContatos

--- Trilha-basico/java-collections-api/MapInterface/src/main/MapInterface.java
+++ Trilha-basico/java-collections-api/MapInterface/src/main/MapInterface.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import MapContato.AgendaContatos;
+
 /**
  *
  * @author breno
@@ -14,7 +16,20 @@ public class MapInterface {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        AgendaContatos agenda = new AgendaContatos();
         
+        agenda.adicionarContato("Camila", 123456);
+        agenda.adicionarContato("Camila", 5665);
+        agenda.adicionarContato("Camila Calvacante", 111111);
+        agenda.adicionarContato("Camila Dio", 654987);
+        agenda.adicionarContato("Maria Silva", 111111);
+        agenda.adicionarContato("Camila", 44444);
+        
+        
+        agenda.exibirContato();
+        agenda.removerContato("Maria Silva");
+        agenda.exibirContato();
+        System.out.println("O numero é: " + agenda.pesquisarPorNome("Camila Dio"));
     }
     
 }
